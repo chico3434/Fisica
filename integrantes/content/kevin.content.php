5e6739f7f961b6e8f12b9ce939b479743061b795
chico3434 <chicokarkow@hotmail.com>
2018-11-13 23:28:23 -0200

    Henrique e Kevin atualizados

--- integrantes/content/kevin.content.php
+++ integrantes/content/kevin.content.php
@@ -1,4 +1,4 @@
-<div class="container mt-2">
+<div class="container mt-2 mb-4">
   <div class="row justify-content-center align-items-center">
     <h1>Trajeto - Kevin Sena de Andrade</h1>
   </div>  
